9ce60d7e43ad0d5abfa351fce48671d0f2c12c82
spinalsubset69 <55968363+SpinalSubset69@users.noreply.github.com>
2025-07-15 22:21:38 -0600

    Feat/test ci (#2)
    * Test CI
    
    * Fix CI file

--- src/InmobiliariaAndes.API/Controllers/WeatherForecastController.cs
+++ src/InmobiliariaAndes.API/Controllers/WeatherForecastController.cs
@@ -20,10 +20,8 @@ namespace InmobiliariaAndes.API.Controllers
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
-        {
-            var newTEst = 100;
-
-            return Enumerable.Range(1, newTEst).Select(index => new WeatherForecast
+        {            
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
