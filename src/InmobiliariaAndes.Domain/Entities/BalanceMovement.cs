799d987c0dfb047dda7ffade103566214e57fd35
spinalsubset69 <55968363+SpinalSubset69@users.noreply.github.com>
2025-07-15 23:33:45 -0600

    Feat/luis garza add models to db context (#6)
    * Test CI (#1)
    
    * Test CI
    
    * Fix CI file
    
    * Feat/merge main into dev (#4)
    
    * test ci
    
    * Feat/test ci (#2)
    
    * Test CI
    
    * Fix CI file
    
    * Create README.md (#3)
    
    * Feat -
    *Add initial entities to project
    *Fix connection string injection

---
+++ src/InmobiliariaAndes.Domain/Entities/BalanceMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace InmobiliariaAndes.Domain.Entities;
+
+[ExcludeFromCodeCoverage]
+public class BalanceMovement
+{
+    public int Socio { get; set; }
+    public string Homoclave { get; set; } = string.Empty;
+    public DateTime Fecha { get; set; }
+    public int Referencia { get; set; }
+    public short Concepto { get; set; }
+    public int Consec { get; set; }
+    public DateTime? FechaVenc { get; set; }
+    public decimal Valor { get; set; }
+    public decimal? Propina { get; set; }
+    public byte? Mesero { get; set; }
+    public byte? TipoOperacion { get; set; }
+    public short? Serie { get; set; }
+    public short? Total { get; set; }
+}
