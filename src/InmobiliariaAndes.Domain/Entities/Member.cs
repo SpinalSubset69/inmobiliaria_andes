9e88567cab5d0d0022218f36de3fa488ef9da3d6
spinalsubset69 <55968363+SpinalSubset69@users.noreply.github.com>
2025-07-15 23:33:11 -0600

    Feat - (#5)
    *Add initial entities to project
    *Fix connection string injection

---
+++ src/InmobiliariaAndes.Domain/Entities/Member.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace InmobiliariaAndes.Domain.Entities;
+
+[ExcludeFromCodeCoverage]
+public class Member
+{
+    public int Socio { get; set; }
+    public string HomoClave { get; set; } = string.Empty;
+    public string Nombre { get; set; } = string.Empty;
+    public string? Paterno { get; set; }
+    public string? Materno { get; set; }
+    public string Sexo { get; set; } = string.Empty;
+    public byte TipoSocio { get; set; }
+    public string? Accion { get; set; }
+    public string? Compania { get; set; }
+    public string? RFC { get; set; }
+    public string? Direccion { get; set; }
+    public string? Colonia { get; set; }
+    public string? Poblacion { get; set; }
+    public int ZonaPostal { get; set; }
+    public string? Direccion_Casa { get; set; }
+    public string? Colonia_Casa { get; set; }
+    public string? Poblacion_Casa { get; set; }
+    public int ZonaPostal_Casa { get; set; }
+    public short ZonaCobranza { get; set; }
+    public short Lada_Casa { get; set; }
+    public string? Telefono_Casa { get; set; }
+    public short Lada_Ofis { get; set; }
+    public string? Telefono_Ofis { get; set; }
+    public DateTime FechaIngreso { get; set; }
+    public byte TipoCobranza { get; set; }
+    public byte? Banco { get; set; }
+    public byte? Sucursal_Banco { get; set; }
+    public string? Cuenta_Banco { get; set; }
+    public byte cveConsumo { get; set; }
+    public string? eMail { get; set; }
+    public string? eMail_Alterno { get; set; }
+    public DateTime? FechaBaja { get; set; }
+    public string? Telefono_Movil { get; set; }
+    public short? Lada_Movil { get; set; }
+}
---
+++ src/InmobiliariaAndes.Infrastructure/DataAccess/DbMappings/ConceptDbMapping.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using InmobiliariaAndes.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InmobiliariaAndes.Infrastructure.DataAccess.DbMappings;
+
+[ExcludeFromCodeCoverage]
+internal static class ConceptDbMapping
+{
+    public static void Map(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Concept>(e =>
+        {
+            e.HasNoKey();
+            e.ToTable("conceptos");
+
+            e.Property(c => c.Concepto).HasColumnName("concepto");
+            e.Property(c => c.Descripcion).HasColumnName("descripcion");
+            e.Property(c => c.DescAbrev).HasColumnName("descabrev");
+            e.Property(c => c.Ptje_IVA).HasColumnName("ptje_iva");
+            e.Property(c => c.CveInt_Morat).HasColumnName("cveint_morat");
+            e.Property(c => c.TipoSaldo).HasColumnName("tiposaldo");
+            e.Property(c => c.TipoConcepto).HasColumnName("tipoconcepto");
+            e.Property(c => c.Caseta).HasColumnName("caseta");
+            e.Property(c => c.Cuota_1).HasColumnName("cuota_1");
+            e.Property(c => c.Cuota_2).HasColumnName("cuota_2");
+            e.Property(c => c.Cuota_3).HasColumnName("cuota_3");
+            e.Property(c => c.Cuota_4).HasColumnName("cuota_4");
+            e.Property(c => c.Cuota_5).HasColumnName("cuota_5");
+            e.Property(c => c.Cuota_6).HasColumnName("cuota_6");
+            e.Property(c => c.Cuota_7).HasColumnName("cuota_7");
+            e.Property(c => c.Cuenta_Cgo).HasColumnName("cuenta_cgo");
+            e.Property(c => c.SubCuenta_Cgo).HasColumnName("subcuenta_cgo");
+            e.Property(c => c.SSubCuenta_Cgo).HasColumnName("ssubcuenta_cgo");
+            e.Property(c => c.SSSubCuenta_Cgo).HasColumnName("sssubcuenta_cgo");
+            e.Property(c => c.Cuenta_Cr).HasColumnName("cuenta_cr");
+            e.Property(c => c.SubCuenta_Cr).HasColumnName("subcuenta_cr");
+            e.Property(c => c.SSubCuenta_Cr).HasColumnName("ssubcuenta_cr");
+            e.Property(c => c.SSSubCuenta_Cr).HasColumnName("sssubcuenta_cr");
+            e.Property(c => c.GenPoliza).HasColumnName("genpoliza");
+        });
+    }
+}
