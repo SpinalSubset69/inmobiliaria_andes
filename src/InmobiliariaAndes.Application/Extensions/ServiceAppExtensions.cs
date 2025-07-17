f52b36a620fbc87ee6fadaa30c7262d80b2bcc2b
SpinalSubset69 <alastorlml@gmail.com>
2025-07-17 17:17:55 -0600

    Update connection string

--- src/InmobiliariaAndes.Application/Extensions/ServiceAppExtensions.cs
+++ src/InmobiliariaAndes.Application/Extensions/ServiceAppExtensions.cs
@@ -3,6 +3,7 @@ using InmobiliariaAndes.Application.Contracts;
 using InmobiliariaAndes.Application.Services;
 using InmobiliariaAndes.Application.Services.HostedServices;
 using InmobiliariaAndes.Infrastructure.Extensions;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,10 +15,21 @@ public static class ServiceAppExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
         // Add Db Service
-        var connectionString =
-            (Environment.GetEnvironmentVariable("Environment") ?? "") == "Local" ?
-            Environment.GetEnvironmentVariable("ConnectionStrings__DbConnectionString") : config.GetConnectionString("DbConnectionString");
-        services.AddAppDbContext(connectionString ?? throw new Exception("Connection string not found"));
+        var dataSource = config.GetValue<string>("DataBase:DataSource") ?? Environment.GetEnvironmentVariable("DATASOURCE");
+        var userId = config.GetValue<string>("DataBase:UserId") ?? Environment.GetEnvironmentVariable("USERID");
+        var password = config.GetValue<string>("DataBase:Password") ?? Environment.GetEnvironmentVariable("PASSWORD");
+        var initialCatalog = config.GetValue<string>("DataBase:InitialCatalog") ?? Environment.GetEnvironmentVariable("INITIALCATALOG");
+
+        var connectionStringBuilder = new SqlConnectionStringBuilder
+        {
+            DataSource = dataSource,
+            UserID = userId,
+            Password = password,
+            InitialCatalog = initialCatalog,
+            TrustServerCertificate = true
+        };
+        
+        services.AddAppDbContext(connectionStringBuilder.ConnectionString ?? throw new Exception("Connection string not found"));
 
         // Add unitOf work
         services.AddTransient<IAppUnitOfWork, AppUnitOfWork>();
